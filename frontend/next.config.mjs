bf00cb13c285cf39819afac199ffba42ec38c33b
Stephen Whitted <EzHelpTechnology@Preciouss-MacBook-Pro.local>
2025-12-05 16:13:40 -0500

    adds

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  images: { unoptimized: true },
+  /* output: "export", -- DISABLED to allow API routes */
+  /* images: { unoptimized: true }, -- DISABLED associated config */
 };
 
 export default nextConfig;
